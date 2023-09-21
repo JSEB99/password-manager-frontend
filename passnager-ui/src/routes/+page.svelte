faabaecfe353a64f9da6d18ce25e7e8b6e18d40c
Sebastian Mora <sebastian.mt99@gmail.com>
2023-09-21 17:57:55 -0500

    first steps

--- passnager-ui/src/routes/+page.svelte
+++ passnager-ui/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>PASSNAGER</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	h1 {
+		font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+		margin: 0 auto;
+		text-align: center;
+	}
+</style>
